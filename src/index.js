09f4c9fb657fbdb2c950f16df1f478115f21b028
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2021-04-04 19:19:20 +0300

    Implement theme.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createMuiTheme, ThemeProvider} from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const darkTheme = createMuiTheme({
+    palette: {
+        type: "dark",
+    }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
