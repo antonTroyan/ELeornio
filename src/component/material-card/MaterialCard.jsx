660a9dac770f395b0edcf05d718ef3f9081e7152
anton.troyan <anton.troyan@gmail.com>
2021-06-03 17:14:06 +0300

    Change links logic. Add button.

--- src/component/material-card/MaterialCard.jsx
+++ src/component/material-card/MaterialCard.jsx
@@ -9,12 +9,13 @@ import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
 import {NavLink} from 'react-router-dom';
 import {Button} from "@material-ui/core";
+import {red} from "@material-ui/core/colors";
 
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        width : 300,
-        height : 250
+        width: 300,
+        height: 250
     },
     paper: {
         padding: theme.spacing(2),
@@ -34,14 +35,12 @@ const MaterialCard = ({id, imageProp, hintProp, titleProp, mainTextProp, linkToM
             <div className={classes.paper}>
                 <Card className={classes.root}>
                     <CardActionArea>
-                        <NavLink to={path}>
                         <CardMedia
                             component="img"
                             height="140"
                             image={imageProp}
                             title={hintProp}
                         />
-                        </NavLink>
                         <CardContent>
                             <Typography gutterBottom variant="h5" component="h2">
                                 {titleProp}
@@ -49,6 +48,9 @@ const MaterialCard = ({id, imageProp, hintProp, titleProp, mainTextProp, linkToM
                             <Typography variant="body2" color="textSecondary" component="p">
                                 {mainTextProp}
                             </Typography>
+                            <NavLink to={path}>
+                                <Button>Exercise</Button>
+                            </NavLink>
                             <Button href={linkToMaterial} color={"secondary"}>
                                 View source
                             </Button>
