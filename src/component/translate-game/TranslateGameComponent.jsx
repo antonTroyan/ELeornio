5324596dbd7d2c2c48865fd679655cbc75f5c25e
anton <antronset@gmail.com>
2021-12-26 16:16:05 +0300

    Revert "Updates"
    This reverts commit 653ba1b6dfaedd9e657e535cfea85da352f1a861.

---
+++ src/component/translate-game/TranslateGameComponent.jsx
@@ -0,0 +1,212 @@
+import React, {useState} from 'react';
+import Button from "@material-ui/core/Button";
+import cloneDeep from 'lodash/cloneDeep';
+import {takeRandomElements, invertRussianEnglish} from "../../util/util";
+import Alert from "@material-ui/lab/Alert";
+import AlertTitle from "@material-ui/lab/AlertTitle";
+import TestResultComponent from "./TestResultComponent";
+import {LinearProgress, ListItem, ListItemText} from "@material-ui/core";
+import {NavLink} from 'react-router-dom';
+
+const styles = {
+    mainContainer: {
+        position: 'absolute',
+        top: '40%',
+        left: '30%',
+        'margin-top': '-50px',
+        'margin-left': '-50px',
+        width: '700px',
+        height: '100px'
+    }
+}
+
+const initialTestResult = {
+    0: undefined,
+    1: undefined,
+    2: undefined,
+    3: undefined,
+    4: undefined,
+    5: undefined,
+    6: undefined,
+    7: undefined,
+    8: undefined,
+    9: undefined,
+    10: undefined
+}
+
+const TranslateGameComponent = (props) => {
+
+    const shuffleUrl = '0';
+    const materialIdUrl = props.match.params.materialId;
+
+    function createArrayAllWords(materials) {
+        let arrayWithAllWords = [];
+        materials.forEach(material => {
+            material.tasks.forEach(task => {
+                arrayWithAllWords.push(task);
+            })
+        })
+        return arrayWithAllWords;
+    }
+
+    const extractCorrectArray = (materials) => {
+        let result;
+        if (materialIdUrl === shuffleUrl) {
+            result = takeRandomElements(10, createArrayAllWords(materials));
+        } else {
+            result = takeRandomElements(10, props.materials[materialIdUrl].tasks);
+        }
+        const shouldInvert = Math.random() < 0.5;
+        if (shouldInvert) {
+            return invertRussianEnglish(result)
+        }
+        return result;
+    }
+
+    const [currentTaskId, setCurrentTaskId] = useState(1)
+    const [testResult, setTestResult] = useState(initialTestResult)
+    const [tasks, setTasks] = useState(extractCorrectArray(props.materials))
+    const [preparedAnswers, setPreparedAnswers] = useState(null)
+    const [russianWord, setRussianWord] = useState(null)
+
+    const clearGameData = () => {
+        setCurrentTaskId(1)
+        setTestResult(initialTestResult)
+        setTasks((extractCorrectArray(props.materials)))
+        setPreparedAnswers(null)
+        setRussianWord(null)
+    }
+
+    const shuffle = (a) => {
+        let j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
+        }
+        return a;
+    }
+
+    function ListItemLink(props) {
+        return <ListItem button component="a" {...props} />;
+    }
+
+    const prepareAnswers = () => {
+        let isCorrectAnswerAlreadyIn = false
+
+        const wrongAnswers = takeRandomElements(4, tasks)
+        const result = wrongAnswers.map((element) => {
+            if (element.correctAnswer === tasks[currentTaskId].correctAnswer) {
+                isCorrectAnswerAlreadyIn = true
+            }
+            return (
+                createAnswerItem(element.correctAnswer)
+            )
+        })
+        const correctAnswer = createAnswerItem(tasks[currentTaskId].correctAnswer)
+        if (!isCorrectAnswerAlreadyIn) {
+            result[0] = correctAnswer
+        }
+
+        return shuffle(result)
+    }
+
+
+    const createAnswerItem = (correctAnswer) => {
+        return (
+            <ListItemLink
+                key={correctAnswer}
+                onClick={handleChooseVariant}>
+                <ListItemText primary={correctAnswer}/>
+            </ListItemLink>
+        )
+    }
+
+    const handleChooseVariant = (e) => {
+        let enteredAnswer = e.currentTarget.textContent
+
+        setTestResult((prev) => {
+            const result = cloneDeep(prev)
+            result[currentTaskId] = enteredAnswer === tasks[currentTaskId].correctAnswer
+            if (result[currentTaskId]) {
+                props.incrementScoreActionCreator()
+                props.decreaseComplexityActionCreator(tasks[currentTaskId].correctAnswer)
+            } else {
+                props.increaseComplexityActionCreator(tasks[currentTaskId].correctAnswer)
+                props.resetScoreActionCreator()
+            }
+            return result
+        })
+    }
+
+    const handleClickNext = () => {
+        setCurrentTaskId((prev) => ++prev)
+        setPreparedAnswers(null)
+        setRussianWord(null)
+    }
+
+    if (currentTaskId >= tasks.length) {
+        return (
+            <TestResultComponent testResult={testResult}
+                                 materialId={materialIdUrl}
+                                 clearGameData={clearGameData}/>
+        )
+    }
+
+    const handlePreparedAnswers = () => {
+        if (preparedAnswers === null) {
+            setPreparedAnswers(prepareAnswers())
+        }
+        return preparedAnswers
+    }
+
+    const handleRussianWord = () => {
+        if (russianWord === null) {
+            setRussianWord(tasks[currentTaskId].russianWord)
+        }
+        return russianWord
+    }
+
+    return (
+        <div>
+            {testResult[currentTaskId] &&
+            <Alert severity="success">
+                <AlertTitle>Success</AlertTitle>
+                You choose right variant — <strong>cool!</strong>
+            </Alert>
+            }
+            {testResult[currentTaskId] === false &&
+            <Alert severity="warning">
+                <AlertTitle>You choose wrong</AlertTitle>
+                The right answer is [<strong>{tasks[currentTaskId].correctAnswer}</strong>]
+            </Alert>
+            }
+
+            <div style={styles.mainContainer}>
+                <p><LinearProgress variant="determinate" value={(currentTaskId - 1) * 10}/></p>
+
+                <div style={{width: '50%', float: 'left', height: '50%'}}>
+                    <ListItemText primary={handleRussianWord()}/>
+                    <h1>{props.score}</h1>
+                </div>
+
+                <div style={{width: '50%', float: 'right'}}>
+                    {handlePreparedAnswers()}
+                    <div style={{margin: 100}}>
+                        {testResult[currentTaskId] !== undefined &&
+                        <Button onClick={handleClickNext}
+                                style={{backgroundColor: 'green'}}
+                                variant="outlined">Next
+                        </Button>}
+                    </div>
+                </div>
+            </div>
+            <NavLink to={"/"}>
+                <Button style={{backgroundColor: 'green'}}>Home Page</Button>
+            </NavLink>
+        </div>
+    )
+}
+
+export default TranslateGameComponent
