f30cec608e2711b35424e704ea27f87905a3ce20
polar <antronset@gmail.com>
2022-01-16 18:55:02 +0300

    Create separate files for materials.

---
+++ src/redux/material/films.jsx
@@ -0,0 +1,34 @@
+
+
+const films = [
+                              // Don't Look Up 2021
+                              {russianWord: 'inquiry', correctAnswer: 'an act of asking for information', complexity: 60},
+                              {russianWord: 'tenured professor', correctAnswer: 'with permanent job contract', complexity: 60},
+                              {russianWord: 'heavy hitter', correctAnswer: 'a very important or influential person', complexity: 60},
+                              {russianWord: 'gesture', correctAnswer: 'a movement of part of the body, especially a hand or the head, to express an idea or meaning', complexity: 60},
+                              {russianWord: 'soothe', correctAnswer: 'gently calm (a person or their feelings)', complexity: 60},
+                              {russianWord: 'fruition', correctAnswer: 'the realization or fulfilment of a plan or project', complexity: 60},
+                              {russianWord: 'relieved', correctAnswer: 'no longer feeling distressed or anxious; reassured', complexity: 60},
+                              // Red notice 2021
+                              {russianWord: 'sought after', correctAnswer: 'much in demand; generally desired', complexity: 60},
+                              {russianWord: 'bejewel', correctAnswer: 'cover or adorn with jewels', complexity: 60},
+                              {russianWord: 'adorn', correctAnswer: 'make more beautiful or attractive', complexity: 60},
+                              {russianWord: 'chaps', correctAnswer: 'leather trousers without a seat, worn by a cowboy over ordinary trousers to protect the legs', complexity: 60},
+                              {russianWord: 'ingress', correctAnswer: 'the action or fact of going in or entering; the capacity or right of entrance', complexity: 60},
+                              {russianWord: 'compel', correctAnswer: 'force or oblige (someone) to do something', complexity: 60},
+                              {russianWord: 'kinks', correctAnswer: 'a mental or physical peculiarity : eccentricity, quirk', complexity: 60},
+                              {russianWord: 'peculiarity', correctAnswer: 'a strange or unusual feature or habit', complexity: 60},
+                              {russianWord: 'quirk', correctAnswer: 'a peculiar aspect of a person\'s character or behaviour', complexity: 60},
+                              {russianWord: 'sparkly', correctAnswer: 'shining with glittering flashes of light / bright', complexity: 60},
+                              {russianWord: 'convict', correctAnswer: 'someone to be guilty of a criminal offence by the verdict of a jury or the decision of a judge in a court of law', complexity: 60},
+                              // Die hard 1988
+                              {russianWord: 'tape deck', correctAnswer: 'a device used to play back and often to record on magnetic tape that usually has to be connected to an audio system', complexity: 60},
+                              {russianWord: 'as tough as nails', correctAnswer: '(of a person) very strong and resilient in the face of hardship or pain', complexity: 60},
+                              {russianWord: 'conceivable', correctAnswer: 'capable of being imagined or grasped mentally', complexity: 60},
+                              {russianWord: 'to be in charge', correctAnswer: 'having control of or responsibility for (something)', complexity: 60},
+                              {russianWord: '', correctAnswer: '', complexity: 60},
+
+                          ]
+
+
+export default films
