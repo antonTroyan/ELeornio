3da7d8597032c6eb1aaf38e75ae6df007d239f3e
a.trayan <anton.troyan7@gmail.com>
2022-01-19 11:58:45 +0300

    Add materials.

--- src/redux/material/series.jsx
+++ src/redux/material/series.jsx
@@ -1,65 +1,155 @@
-
-
 const series = [
-                               // Cowboy Bebop 2021
-                               {russianWord: 'team up', correctAnswer: 'to join with someone to work together', complexity: 60},
-                               {russianWord: 'tie', correctAnswer: 'a piece of string, cord, or similar used for fastening or tying something', complexity: 60},
-                               {russianWord: 'fastening', correctAnswer: 'a device that closes or secures something', complexity: 60},
-                               {russianWord: 'cord', correctAnswer: 'thin, flexible string or rope made from several twisted strands', complexity: 60},
-                               {russianWord: 'string', correctAnswer: 'material consisting of threads of cotton, hemp, or other material twisted together to form a thin length.', complexity: 60},
-                               {russianWord: 'hemp', correctAnswer: 'the cannabis plant, especially when grown for fibre', complexity: 60},
-                               {russianWord: 'thin', correctAnswer: 'with opposite surfaces or sides that are close or relatively close together', complexity: 60},
-                               {russianWord: 'nab', correctAnswer: 'catch (someone) doing something wrong', complexity: 60},
-                               {russianWord: 'contempt', correctAnswer: 'the feeling that a person or a thing is worthless or beneath consideration', complexity: 60},
-                               {russianWord: 'chump', correctAnswer: 'a foolish or easily deceived person', complexity: 60},
-                               {russianWord: 'chump change', correctAnswer: 'a small or insignificant amount of money', complexity: 60},
-                               {russianWord: 'chop', correctAnswer: 'cut (something) into pieces with repeated sharp blows of an axe or knife', complexity: 60},
-                               {russianWord: 'mole', correctAnswer: 'a small burrowing mammal with dark velvety fur, a long muzzle, and very small eyes', complexity: 60},
-                               {russianWord: 'burrow', correctAnswer: '(of an animal) make a hole or tunnel, typically for use as a dwelling', complexity: 60},
-                               {russianWord: 'dwelling', correctAnswer: 'a house, flat, or other place of residence', complexity: 60},
-                               {russianWord: 'nickel and dime', correctAnswer: 'unimportant / insignificant', complexity: 60},
-                               {russianWord: 'mercenary', correctAnswer: 'primarily concerned with making money at the expense of ethics', complexity: 60},
-                               {russianWord: 'vile', correctAnswer: 'morally bad; wicked', complexity: 60},
-                               {russianWord: 'moniker', correctAnswer: 'a person\'s name, especially a nickname or alias', complexity: 60},
-                               {russianWord: 'loofah', correctAnswer: 'the fibrous interior of a fruit that resembles a marrow, dried and used as a sponge for washing the body', complexity: 60},
-                               {russianWord: 'duct tape', correctAnswer: 'strong cloth-backed waterproof adhesive tape', complexity: 60},
-                               {russianWord: 'tape', correctAnswer: 'a narrow strip of material, typically used to hold or fasten something.', complexity: 60},
-                               {russianWord: 'adhesive', correctAnswer: 'a substance used for sticking objects or materials together; glue', complexity: 60},
-                               {russianWord: 'wrench', correctAnswer: 'adjustable metal tool used for tightening or loosening metal nuts of different sizes. ', complexity: 60},
-                               {russianWord: 'ham', correctAnswer: 'salted or smoked meat from the upper part of a pig\'s leg', complexity: 60},
-                               {russianWord: 'rustle up', correctAnswer: 'to make, find, or prepare something quickly', complexity: 60},
-                               {russianWord: 'scam', correctAnswer: 'a dishonest scheme; a fraud', complexity: 60},
-                               {russianWord: 'pipe dream', correctAnswer: 'an unattainable or fanciful hope or scheme', complexity: 60},
-                               {russianWord: 'hustle', correctAnswer: 'a state of great activity', complexity: 60},
-                               {russianWord: 'interstellar', correctAnswer: 'occurring or situated between stars', complexity: 60},
-                               {russianWord: 'kink', correctAnswer: 'a sharp twist or curve in something that is otherwise straight', complexity: 60},
-                               {russianWord: 'recital', correctAnswer: 'a performance of a programme of music', complexity: 60},
-                               {russianWord: 'be on the lam ', correctAnswer: 'running away / in escape status.', complexity: 60},
-                               {russianWord: 'lewd', correctAnswer: 'crude and offensive in a sexual way', complexity: 60},
-                               {russianWord: 'vagary', correctAnswer: 'an unexpected and inexplicable change in a situation or in someone\'s behaviour', complexity: 60},
-                               {russianWord: 'minutiae', correctAnswer: 'the small, precise, or trivial details of something', complexity: 60},
-                               // Witcher
-                               {russianWord: 'lodgings', correctAnswer: 'temporary accommodation', complexity: 60},
-                               {russianWord: 'keep low profile', correctAnswer: 'stay out of public notice, avoid attracting attention to oneself', complexity: 60},
-                               {russianWord: 'bliss', correctAnswer: 'perfect happiness; great joy', complexity: 60},
-                               {russianWord: 'chap', correctAnswer: 'guy or a fellow — a boy or man who\'s a friend, acquaintance, or a friendly stranger', complexity: 60},
-                               {russianWord: 'lame', correctAnswer: '(especially of an animal) unable to walk without difficulty as the result of an injury', complexity: 60},
-                               {russianWord: 'redeem', correctAnswer: 'compensate for the faults or bad aspects of', complexity: 60},
-                               {russianWord: 'den', correctAnswer: 'a wild mammal\'s hidden home; a lair', complexity: 60},
-                               {russianWord: 'codger', correctAnswer: 'a man, esp an old or eccentric one', complexity: 60},
-                               {russianWord: 'beg', correctAnswer: 'ask someone earnestly or humbly for something', complexity: 60},
-                               {russianWord: 'hesitation', correctAnswer: 'the action of pausing before saying or doing something', complexity: 60},
-                               {russianWord: 'lurk', correctAnswer: 'be or remain hidden so as to wait in ambush for someone or something', complexity: 60},
-                               {russianWord: 'ambush', correctAnswer: 'a surprise attack by people lying in wait in a concealed position', complexity: 60},
-                               {russianWord: 'concealed', correctAnswer: 'kept secret; hidden', complexity: 60},
-                               {russianWord: 'hatred', correctAnswer: 'intense dislike; hate', complexity: 60},
-                               {russianWord: 'conduit', correctAnswer: 'a channel for conveying water or other fluid', complexity: 60},
-                               {russianWord: 'gossip', correctAnswer: 'casual conversation about other people, typically involving details that are not confirmed as being true', complexity: 60},
-                               {russianWord: 'flee', correctAnswer: 'run away from a place or situation of danger', complexity: 60},
-                               {russianWord: '', correctAnswer: '', complexity: 60},
-                               {russianWord: '', correctAnswer: '', complexity: 60},
-                               {russianWord: '', correctAnswer: '', complexity: 60},
-                               {russianWord: '', correctAnswer: '', complexity: 60},
-                               ]
+    // Cowboy Bebop 2021
+    {russianWord: 'team up', correctAnswer: 'to join with someone to work together', complexity: 60},
+    {
+        russianWord: 'tie',
+        correctAnswer: 'a piece of string, cord, or similar used for fastening or tying something',
+        complexity: 60
+    },
+    {russianWord: 'fastening', correctAnswer: 'a device that closes or secures something', complexity: 60},
+    {
+        russianWord: 'cord',
+        correctAnswer: 'thin, flexible string or rope made from several twisted strands',
+        complexity: 60
+    },
+    {
+        russianWord: 'string',
+        correctAnswer: 'material consisting of threads of cotton, hemp, or other material twisted together to form a thin length.',
+        complexity: 60
+    },
+    {russianWord: 'hemp', correctAnswer: 'the cannabis plant, especially when grown for fibre', complexity: 60},
+    {
+        russianWord: 'thin',
+        correctAnswer: 'with opposite surfaces or sides that are close or relatively close together',
+        complexity: 60
+    },
+    {russianWord: 'nab', correctAnswer: 'catch (someone) doing something wrong', complexity: 60},
+    {
+        russianWord: 'contempt',
+        correctAnswer: 'the feeling that a person or a thing is worthless or beneath consideration',
+        complexity: 60
+    },
+    {russianWord: 'chump', correctAnswer: 'a foolish or easily deceived person', complexity: 60},
+    {russianWord: 'chump change', correctAnswer: 'a small or insignificant amount of money', complexity: 60},
+    {
+        russianWord: 'chop',
+        correctAnswer: 'cut (something) into pieces with repeated sharp blows of an axe or knife',
+        complexity: 60
+    },
+    {
+        russianWord: 'mole',
+        correctAnswer: 'a small burrowing mammal with dark velvety fur, a long muzzle, and very small eyes',
+        complexity: 60
+    },
+    {
+        russianWord: 'burrow',
+        correctAnswer: '(of an animal) make a hole or tunnel, typically for use as a dwelling',
+        complexity: 60
+    },
+    {russianWord: 'dwelling', correctAnswer: 'a house, flat, or other place of residence', complexity: 60},
+    {russianWord: 'nickel and dime', correctAnswer: 'unimportant / insignificant', complexity: 60},
+    {
+        russianWord: 'mercenary',
+        correctAnswer: 'primarily concerned with making money at the expense of ethics',
+        complexity: 60
+    },
+    {russianWord: 'vile', correctAnswer: 'morally bad; wicked', complexity: 60},
+    {russianWord: 'moniker', correctAnswer: 'a person\'s name, especially a nickname or alias', complexity: 60},
+    {
+        russianWord: 'loofah',
+        correctAnswer: 'the fibrous interior of a fruit that resembles a marrow, dried and used as a sponge for washing the body',
+        complexity: 60
+    },
+    {russianWord: 'duct tape', correctAnswer: 'strong cloth-backed waterproof adhesive tape', complexity: 60},
+    {
+        russianWord: 'tape',
+        correctAnswer: 'a narrow strip of material, typically used to hold or fasten something.',
+        complexity: 60
+    },
+    {
+        russianWord: 'adhesive',
+        correctAnswer: 'a substance used for sticking objects or materials together; glue',
+        complexity: 60
+    },
+    {
+        russianWord: 'wrench',
+        correctAnswer: 'adjustable metal tool used for tightening or loosening metal nuts of different sizes. ',
+        complexity: 60
+    },
+    {russianWord: 'ham', correctAnswer: 'salted or smoked meat from the upper part of a pig\'s leg', complexity: 60},
+    {russianWord: 'rustle up', correctAnswer: 'to make, find, or prepare something quickly', complexity: 60},
+    {russianWord: 'scam', correctAnswer: 'a dishonest scheme; a fraud', complexity: 60},
+    {russianWord: 'pipe dream', correctAnswer: 'an unattainable or fanciful hope or scheme', complexity: 60},
+    {russianWord: 'hustle', correctAnswer: 'a state of great activity', complexity: 60},
+    {russianWord: 'interstellar', correctAnswer: 'occurring or situated between stars', complexity: 60},
+    {
+        russianWord: 'kink',
+        correctAnswer: 'a sharp twist or curve in something that is otherwise straight',
+        complexity: 60
+    },
+    {russianWord: 'recital', correctAnswer: 'a performance of a programme of music', complexity: 60},
+    {russianWord: 'be on the lam ', correctAnswer: 'running away / in escape status.', complexity: 60},
+    {russianWord: 'lewd', correctAnswer: 'crude and offensive in a sexual way', complexity: 60},
+    {
+        russianWord: 'vagary',
+        correctAnswer: 'an unexpected and inexplicable change in a situation or in someone\'s behaviour',
+        complexity: 60
+    },
+    {
+        russianWord: 'minutiae [миньюшиай]',
+        correctAnswer: 'the small, precise, or trivial details of something',
+        complexity: 60
+    },
+    // Witcher
+    {russianWord: 'lodgings', correctAnswer: 'temporary accommodation', complexity: 60},
+    {
+        russianWord: 'keep low profile',
+        correctAnswer: 'stay out of public notice, avoid attracting attention to oneself',
+        complexity: 60
+    },
+    {russianWord: 'bliss', correctAnswer: 'perfect happiness; great joy', complexity: 60},
+    {
+        russianWord: 'chap',
+        correctAnswer: 'guy or a fellow — a boy or man who\'s a friend, acquaintance, or a friendly stranger',
+        complexity: 60
+    },
+    {
+        russianWord: 'lame',
+        correctAnswer: '(especially of an animal) unable to walk without difficulty as the result of an injury',
+        complexity: 60
+    },
+    {russianWord: 'redeem', correctAnswer: 'compensate for the faults or bad aspects of', complexity: 60},
+    {russianWord: 'den', correctAnswer: 'a wild mammal\'s hidden home; a lair', complexity: 60},
+    {russianWord: 'codger', correctAnswer: 'a man, esp an old or eccentric one', complexity: 60},
+    {russianWord: 'beg', correctAnswer: 'ask someone earnestly or humbly for something', complexity: 60},
+    {
+        russianWord: 'hesitation',
+        correctAnswer: 'the action of pausing before saying or doing something',
+        complexity: 60
+    },
+    {
+        russianWord: 'lurk',
+        correctAnswer: 'be or remain hidden so as to wait in ambush for someone or something',
+        complexity: 60
+    },
+    {
+        russianWord: 'ambush',
+        correctAnswer: 'a surprise attack by people lying in wait in a concealed position',
+        complexity: 60
+    },
+    {russianWord: 'concealed', correctAnswer: 'kept secret; hidden', complexity: 60},
+    {russianWord: 'hatred', correctAnswer: 'intense dislike; hate', complexity: 60},
+    {russianWord: 'conduit', correctAnswer: 'a channel for conveying water or other fluid', complexity: 60},
+    {
+        russianWord: 'gossip',
+        correctAnswer: 'casual conversation about other people, typically involving details that are not confirmed as being true',
+        complexity: 60
+    },
+    {russianWord: 'flee', correctAnswer: 'run away from a place or situation of danger', complexity: 60},
+    {russianWord: '', correctAnswer: '', complexity: 60},
+    {russianWord: '', correctAnswer: '', complexity: 60},
+    {russianWord: '', correctAnswer: '', complexity: 60},
+    {russianWord: '', correctAnswer: '', complexity: 60},
+]
 
 export default series
