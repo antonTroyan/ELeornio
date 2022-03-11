f7203cce3115dc079d722abf551693a113f1351b
polar <antronset@gmail.com>
2022-03-11 20:43:59 +0300

    Refactor.

--- src/util/util.jsx
+++ src/util/util.jsx
@@ -1,9 +1,12 @@
+const DEFAULT_COMPLEXITY = 60
+
 export const takeRandomElements = (numberOfElements, array) => {
     const result = []
 
     for (let i = 0; i <= numberOfElements;) {
         const randomElementIndex = Math.floor(Math.random() * array.length)
         const randomElement = array[randomElementIndex]
+        randomElement.complexity = DEFAULT_COMPLEXITY
         if (!result.includes(array[randomElementIndex])
             && randomElement.correctAnswer !== ''
             && Math.floor(Math.random() * 100) < randomElement.complexity) {
